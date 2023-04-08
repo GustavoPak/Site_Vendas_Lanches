6fa33896a325cc58bd5874428b8902b4b2172a5f
GustavoPak <gutskinaruto159@gmail.com>
2023-04-08 09:15:05 -0300

    first commit

---
+++ Vendas_Site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vendas_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
